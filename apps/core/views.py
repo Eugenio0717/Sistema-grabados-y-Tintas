d124e8f16f47fef44b25d5e405c83bc94aa23a31
Eugenio <eugenio.software17@gmail.com>
2026-03-16 07:24:44 -0400

    Cambios de sidebar-01

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required # Importamos el decorador
 
+def dashboard(request):
+    return render(request, 'dashboard.html')
+@login_required # ¡Aquí está el guardia de seguridad!
 def dashboard(request):
     return render(request, 'dashboard.html')
